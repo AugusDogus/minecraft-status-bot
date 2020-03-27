4801101d6f0f3f8d4c1cbe4ceecd06c83efd1f2d
Augie <augie@luebbers.email>
2020-03-27 13:16:54 -0500

    Added role to show who's playing Minecraft

--- app.js
+++ app.js
@@ -30,9 +30,69 @@ const setActivity = ((status) => {
         });
 });
 
-// Set the refresh refresh rate for the bot's status
 client.on('ready', () => {
+    // Set the refresh refresh rate for the bot's status
     client.setInterval(update, process.env.delay || 30000);
+    // If we're added to the server, check if the roll exists.
+    let guild = client.guilds.get('546494699997888542');
+    if (guild) {
+        if (!guild.roles.find(role => role.name === "Playing Minecraft")) {
+            // Add the roll to the server.
+            guild.createRole({
+                hoist: true,
+                name: 'Playing Minecraft',
+            }).then(role => {
+                process.env.roleID = role.id;
+            }).catch(error => {
+                if (error == 'DiscordAPIError: Missing Permissions') {
+                    console.error('Please give the bot Administrator privileges and restart.');
+                }
+            });
+        } else {
+            process.env.roleID = guild.roles.find(role => role.name === 'Playing Minecraft').id;
+        }
+    } else {
+        console.error('Please add the bot to your server and restart.');
+    }
+    // Update everyone's role status from while the bot was starting/restarting.
+    guild.members.forEach((member) => {
+        // Check if they are playing a game
+        if (member.presence.game) {
+            // Check if the game is Minecraft
+            if (member.presence.game.name === 'Minecraft') {
+                // Give them the Playing Minecraft role
+                member.addRole(process.env.roleID);
+            }
+        } else {
+            member.removeRole(process.env.roleID);
+        }
+    });
+});
+
+// Check status
+client.on('presenceUpdate', (old, member) => {
+    // Check if they started playing a game
+    if (member.presence.game) {
+        // Check if the game is Minecraft
+        if (member.presence.game.name === 'Minecraft') {
+            // Give them the Playing Minecraft role
+            member.addRole(process.env.roleID);
+        }
+    }
+    // Check if they stopped playing a game
+    if (old.presence.game) {
+        // Check if the game was Minecraft Legends
+        if (old.presence.game.name === 'Minecraft') {
+            // Make sure they're not still playing Minecraft Legends
+            if (member.presence.game) {
+                if (member.presence.game.name !== 'Minecraft') {
+                    member.removeRole(process.env.roleID);
+                }
+            } else {
+                member.removeRole(process.env.roleID);
+            }
+        }
+    }
 });
 
 // Login to discord api
